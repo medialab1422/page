6b975c43e0d3e51690cb4862ca5c9c1bc2aa5165
95leohuang <leohuang95@gmail.com>
2022-09-18 18:45:47 +0800

    【Feat】 - iosWeb optimize YouTube loading

--- iosWeb/script.js
+++ iosWeb/script.js
@@ -93,10 +93,51 @@ function ShowYearDemo(current_year) {
             var sectionHtml = `
                 <div id="` + i + `" class="overlay reveal " style="background-color:` + randomColor() + `">
                     <h4 class="heading">` + allData[i].title + `</h4>
-                   
-                    <iframe id="ytplayer" type="text/html" width="100%" height="640"
-                    src="https://www.youtube.com/embed/` + allData[i].video + `?autoplay=1"
-                    frameborder="0"></iframe>
+                    <iframe 
+                    style=" top: 0; left:0; width: 100%; height: 640px; border: 0;"
+                    loading="lazy";
+                    srcdoc="<style>
+                      * {
+                      padding: 0;
+                      margin: 0;
+                      overflow: hidden;
+                      }
+                      
+                      body, html {
+                        height: 100%;
+                        background-color:black;
+                      }
+                      
+                      img, svg {
+                        position: absolute;
+                        width: 100%;
+                        top: 0;
+                        bottom: 0;
+                        margin: auto;
+                      }
+                      
+                      svg {
+                        filter: drop-shadow(1px 1px 10px hsl(206.5, 70.7%, 8%));
+                        transition: all 250ms ease-in-out;
+                      }
+                      
+                      body:hover svg {
+                        filter: drop-shadow(1px 1px 10px hsl(206.5, 0%, 10%));
+                        transform: scale(1.2);
+                      }
+                    </style>
+                    <a href='https://www.youtube.com/embed/${allData[i].video}?autoplay=1'>
+                      <img src='https://img.youtube.com/vi/${allData[i].video}/hqdefault.jpg' alt='Coffee Recipe Javascript Project' >
+                      <svg xmlns='http://www.w3.org/2000/svg' width='64' height='64' viewBox='0 0 24 24' fill='none' stroke='#ffffff' stroke-width='2' stroke-linecap='round' stroke-linejoin='round' class='feather feather-play-circle'><circle cx='12' cy='12' r='10'></circle><polygon points='10 8 16 12 10 16 10 8'></polygon></svg>
+                    </a>
+                    "
+                    src="https://www.youtube.com/embed/${allData[i].video}" 
+                    title="Coffee Recipe Javascript Project"
+                    frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                    allowfullscreen>
+                  </iframe>
+
                     <div class="col-md-2"></div>
                     
                     <div class="col-md-8">
